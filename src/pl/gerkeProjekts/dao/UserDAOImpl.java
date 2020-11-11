5bdd6193f943cafc3a38a022e7badbb4b58bc9d7
Piotr <piotrg2003@gmail.com>
2020-11-11 20:57:22 +0100

    Login page config

--- src/pl/gerkeProjekts/dao/UserDAOImpl.java
+++ src/pl/gerkeProjekts/dao/UserDAOImpl.java
@@ -77,14 +77,16 @@ public class UserDAOImpl implements UserDAO {
     }
 
     private class UserRowMapper implements RowMapper<User> {
+
         @Override
-        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
             User user = new User();
             user.setId(resultSet.getLong("user_id"));
             user.setUsername(resultSet.getString("username"));
-            user.setPassword(resultSet.getString("password"));
             user.setEmail(resultSet.getString("email"));
+            user.setPassword(resultSet.getString("password"));
             return user;
         }
+
     }
 }
